9093badb697b98d90c2da637b5493443bf53ac89
Ken Nguyen <nguyenquangduc.khtc@gmail.com>
2023-04-21 10:23:05 +0700

    project humman

---
+++ script.js
@@ -0,0 +1,17 @@
+
+  
+       
+ window.addEventListener("load",function(event){
+  
+
+            var loading = document.querySelector('#loading');
+           
+             var contents = document.querySelector('.content-loading');
+            console.log(contents);
+            contents.style.display = 'block';
+            loading.style.display = 'none';
+   
+        })
+      
+    
+
